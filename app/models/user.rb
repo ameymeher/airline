b02a763dc558caa49406ab79d210685296289866
Pujitha Enamandala <pujithae@Pujithas-MacBook-Air.local>
2022-09-30 15:48:26 -0400

    Add validations for user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   has_many :reservations
   has_many :baggages
-  validates :email, presence: true, uniqueness: true
+  validates :credit_card_no, length: {is: 19}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  validates :mobile, length: {is: 10}, format: { with: /\A\d+\z/}
 end
